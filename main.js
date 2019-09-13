44e6a6a0cbf159f2be760556a4055b52da7c046c
Iwanaga <iwanaga@IwanaganoMacBook-Air.local>
2019-09-13 22:38:25 +0900

    指摘分修正しました。

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-const midashi = document.getElementById("title");
-midashi.textContent = "JSで変更した後のタイトル";
+const content_Title = document.getElementById("title");
+content_Title.textContent = "JSで変更した後のタイトル";
 
 /**
  * 課題2: p要素でid属性値が `description` のテキスト内容を変更する
@@ -21,12 +21,12 @@ setumei.innerHTML = "JSで変更した後の<strong>説明文</strong>";
  *     - 自分で用意する
  *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
  */
-const neko = document.getElementById("profile")
-neko.setAttribute("src", "https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg");
+const picture = document.getElementById("profile")
+picture.setAttribute("src", "https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg");
 
 /**
  * 課題4: 課題1で作成したh1要素のDOMにクラス属性値をセットする
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-midashi.className = "bg-yellow";
+content_Title.className = "bg-yellow";
